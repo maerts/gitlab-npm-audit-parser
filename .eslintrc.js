b951fcaa272ecf58ae53468867d9a9e28a2dd248
codejedi365 <codejedi365@gmail.com>
2021-05-16 18:17:52 -0400

    devtool-eslint > apply lint standards across codebase

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,14 @@
 module.exports = {
   env: {
     commonjs: true,
-    es2017: true,
+    es2017: true
   },
-  extends: [
-    "eslint:recommended",
-    "airbnb-base",
-    "plugin:prettier/recommended"
-  ],
+  extends: ["eslint:recommended", "airbnb-base", "plugin:prettier/recommended"],
   parserOptions: {
-    ecmaVersion: 8,
+    ecmaVersion: 8
   },
-  ignorePatterns: [ "dist/**" ],
-  rules: {}
+  ignorePatterns: ["dist/**"],
+  rules: {
+    "no-console": "off"
+  }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const isProduction = process.env.NODE_ENV == "production";
-const thisModule = require("./package.json");
+const isProduction = process.env.NODE_ENV === "production";
 const path = require("path");
 const SheBangPlugin = require("webpack-shebang-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
+const thisModule = require("./package.json");
 
 const config = {
   entry: "./parse.js",
   target: "node",
   output: {
-    path: path.resolve(__dirname, path.dirname(thisModule.main) === "." ? "dist" : path.dirname(thisModule.main)),
+    path: path.resolve(
+      __dirname,
+      path.dirname(thisModule.main) === "."
+        ? "dist"
+        : path.dirname(thisModule.main)
+    ),
     filename: path.basename(thisModule.main)
   },
   plugins: [
