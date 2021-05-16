36ec45af7e3e1e15841ad81a9d966eb8cc35dcd6
codejedi365 <codejedi365@gmail.com>
2021-05-16 18:26:50 -0400

    devtool-eslint > enable `--watch` to use lint fixes
    ### Rationale
    There is a flaw in eslint-webpack-plugin that caches the lint
    errors while webpack is configured to watch the directory
    This helps detect when it is in watch mode and set fix to true which
    provides the expected interaction

--- webpack.config.js
+++ webpack.config.js
@@ -6,36 +6,41 @@ const SheBangPlugin = require("webpack-shebang-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const thisModule = require("./package.json");
 
-const config = {
-  entry: "./parse.js",
-  target: "node",
-  output: {
-    path: path.resolve(
-      __dirname,
-      path.dirname(thisModule.main) === "."
-        ? "dist"
-        : path.dirname(thisModule.main)
-    ),
-    filename: path.basename(thisModule.main)
-  },
-  plugins: [
-    new SheBangPlugin({
-      chmod: 0o755
-    }),
-    new ESLintPlugin()
-  ],
-  optimization: {
-    // minimize & mangle the output files (TerserPlugin w/ webpack@v5)
-    minimize: true,
-    // determine which exports are used by modules and removed unused ones
-    usedExports: true
-  },
-  resolve: {
-    extensions: [".js", ".json"]
-  }
-};
+function buildConfig(env) {
+  return {
+    entry: "./parse.js",
+    target: "node",
+    output: {
+      path: path.resolve(
+        __dirname,
+        path.dirname(thisModule.main) === "."
+          ? "dist"
+          : path.dirname(thisModule.main)
+      ),
+      filename: path.basename(thisModule.main)
+    },
+    plugins: [
+      new SheBangPlugin({
+        chmod: 0o755
+      }),
+      new ESLintPlugin({
+        fix: env.WEBPACK_WATCH
+      })
+    ],
+    optimization: {
+      // minimize & mangle the output files (TerserPlugin w/ webpack@v5)
+      minimize: true,
+      // determine which exports are used by modules and removed unused ones
+      usedExports: true
+    },
+    resolve: {
+      extensions: [".js", ".json"]
+    }
+  };
+}
 
-module.exports = () => {
+module.exports = (env) => {
+  const config = buildConfig(env);
   if (isProduction) {
     config.mode = "production";
   } else {
